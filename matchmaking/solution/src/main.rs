b0b0184737d6e37382b6e74ca13b9c012d86f01c
pacable <igor.mamaev1@gmail.com>
2025-07-20 13:52:17 +0300

    Correctly set up logging

--- matchmaking/solution/src/main.rs
+++ matchmaking/solution/src/main.rs
@@ -1,19 +1,19 @@
 mod services;
 
-use std::string::ToString;
-use tracing::{debug, error, info};
+use tracing::{debug, error, info, warn};
 use tracing_subscriber;
-use reqwest;
 
 use std::thread;
 use std::sync::{ Arc, Mutex };
 use std::time::Duration;
-use reqwest::blocking::Client;
+use reqwest::blocking::*;
 
 const SERVER_NAME: &str = "127.0.0.1:8000";
 
 fn main() {
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG) // override log level from env variable
+        .init();
     info!("Solution set up");
 
     let client = Arc::new(Client::new());
@@ -28,11 +28,11 @@ fn main() {
             debug!("Attempting to ping test system");
             let url = get_url("/ping");
             for attempt_number in 1..=10 {
-                info!("Ping attempt #{}", attempt_number);
+                debug!("Ping attempt #{}", attempt_number);
                 match client_ref.get(&url).send() {
                     Ok(response) => {
                         if response.status().is_success() {
-                            info!("Successfully pinged {}. (Status: {})", url, response.status());
+                            debug!("Successfully pinged {}. (Status: {})", url, response.status());
                             let mut success_lock = success
                                 .lock()
                                 .expect("failed to unwrap success lock");
@@ -43,7 +43,7 @@ fn main() {
                         }
                     }
                     Err(e) => {
-                        error!("Error fetching {}: {}", url, e);
+                        warn!("Error fetching {}: {}", url, e);
                         thread::sleep(Duration::from_secs(1));
                     }
                 }
@@ -56,9 +56,11 @@ fn main() {
         return;
     }
 
+    info!("Connection to test system running on {} set up successfully", SERVER_NAME);
+
 
 }
 
 fn get_url(endpoint_path: &str) -> String {
-    SERVER_NAME.to_string() + endpoint_path
+    String::from("http://") + SERVER_NAME + endpoint_path
 }
